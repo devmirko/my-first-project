1d4f94de47f6e78b4d203da840ef19737f36bc07
Simonetti Luigi Mirko <mirko.simonetti.ta@gmail.com>
2022-12-06 12:00:34 +0100

    passaggio oggetto da parent a child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-first-project';
 
+  comix = [{
+    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+    price: "$19.99",
+    series: "Action Comics",
+    type: "comic book"
+  },
+  {
+    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+    price: "$3.99",
+    series: "American Vampire 1976",
+    type: "comic book"
+  },
+
+]
+
   onInput( event: Event ) {
     this.title = (<HTMLInputElement>event.target).value
     console.log((<HTMLInputElement>event.target).value)
--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
@@ -6,23 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prova.component.css']
 })
 export class ProvaComponent implements OnInit {
-
+  @Input() data: any
   isdisable = false
 
- comix = [{
-    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
-    price: "$19.99",
-    series: "Action Comics",
-    type: "comic book"
-  },
-  {
-    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
-    price: "$3.99",
-    series: "American Vampire 1976",
-    type: "comic book"
-  },
 
-]
 
   constructor() {
 
