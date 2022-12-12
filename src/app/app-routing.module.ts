b77ed4718d927db12f0266db8b800d172b69c675
Simonetti Luigi Mirko <mirko.simonetti.ta@gmail.com>
2022-12-12 16:15:50 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AuthGuard } from './auth/auth.guard';
 import { ContactComponent } from './contact/contact.component';
 import { FumettiComponent } from './fumetti/fumetti.component';
 import { NotfoundComponent } from './notfound/notfound.component';
@@ -9,7 +10,7 @@ const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo: '/fumetti' },
   {
     path: 'fumetti',
-    component: FumettiComponent
+    component: FumettiComponent, canActivate: [AuthGuard]
   },
   {
     path: 'fumetti/:id',
@@ -27,8 +28,7 @@ const routes: Routes = [
     path: '404',
     component: NotfoundComponent
   },
-  {
-    path: '**', redirectTo:'/404'},
+  {path: '**', redirectTo:'/404'},
 ];
 
 @NgModule({
