091d993622308a0ab5d89047b86ccf828c957c0c
Simonetti Luigi Mirko <mirko.simonetti.ta@gmail.com>
2022-12-15 09:52:12 +0100

    problema ID risolto

--- src/app/fumetto/fumetto.component.ts
+++ src/app/fumetto/fumetto.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseService } from '../Service/firebase.service';
 
 @Component({
   selector: 'app-fumetto',
   templateUrl: './fumetto.component.html',
   styleUrls: ['./fumetto.component.css']
 })
-export class FumettoComponent {
-@Input() fumetto: any;
+export class FumettoComponent implements OnInit {
+fumetto: any;
 
-  constructor(){}
+  constructor(private firebase: FirebaseService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => this.getFumettoById(params['id']))
+  }
+
+  getFumettoById(id: string){
+    this.firebase.getFumetto(this.firebase.urlFumetti,id)
+    .subscribe((data : any) =>
+      this.fumetto = data);
+    }
+  }
 
 
 
-}
+
